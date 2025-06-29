5c89a835c126967de6d962aadc47c6dbc6c382bd
DanielRodr1 <drodrigueza10@upao.edu.pe>
2025-06-29 18:53:49 -0500

    ADD Logs

--- app/api.py
+++ app/api.py
@@ -56,4 +56,6 @@ async def analyze_skin(
         return result
 
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=f"Error al procesar la predicción: {str(e)}")
